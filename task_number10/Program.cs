edd446deea4dd8db6fc8e515c362d0d3882ace72
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-19 11:03:03 +0500

    task_number10

---
+++ task_number10/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5     782 -> 8     918 -> 1
+
+int Promt(string message)
+{
+    Console.Write(message);
+    String value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int number = Promt("Введите трехзначное число: ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число пожалуйста повторите ввод ");
+    return;
+}
+ Console.WriteLine($"Введенное число {number}");
+int secondRank = number /10 % 10;
+Console.WriteLine($"Вторая цифра {secondRank}");
