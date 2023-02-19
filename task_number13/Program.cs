4cb689b3be2d5d7940d9c2605a6494817aa92052
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-19 14:53:29 +0500

    task_number13

---
+++ task_number13/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5    // 78 -> третьей цифры нет    // 32679 -> 6
+
+int Promt(string message)
+{
+    Console.Write(message);
+    String value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int GetThirdRank(int number)
+{
+    while(number > 999)
+   {
+    number /= 10;
+   }
+return number % 10;
+}
+bool ValidateNumber(int number)
+{
+    if (number <1000)
+{
+    Console.WriteLine("Третьей цифры нет");
+    return false;
+}
+return true;
+}
+int number =Promt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
